29f2bc24de213e45c8ecbc038342d0c895cf6873
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-22 04:14:21 +0700

    Add greeting based on system time in fungsi1a.php

---
+++ tipTrikPemrogramanPhp/fungsi1a.php
@@ -0,0 +1,12 @@
+// Tanpa menggunakan fungsi
+
+/* Mendapatkan jam dari sistem dengan format 0-23 */
+$time = date('G');
+if ($time < 12){
+  $Say = "Good morning";
+} elseif ($time < 17) {
+  $say = "Good Afternoon";
+} else {
+  $say = "Good evening";
+}
+echo $say;
