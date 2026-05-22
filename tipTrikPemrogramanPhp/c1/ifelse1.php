37ea10bb1980a37a35e08c0bfe0407122ba513bd
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-22 10:31:46 +0700

    Create ifelse1.php

---
+++ tipTrikPemrogramanPhp/c1/ifelse1.php
@@ -0,0 +1,12 @@
+<?php
+// Statement if else
+
+/* pernyataan kondisional if Dan else */
+$pass = 'okey';
+if ($pass == 'oke'){ 
+  echo 'OKE <br≥';
+} else {
+  echo 'NOT OKE <br≥';
+}
+// Hasil: NOT OKE
+?>
