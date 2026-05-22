932f3fd327e91072780fd67e4ea53330079737fd
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-22 10:56:49 +0700

    Create ifelse2b.php for ternary operator example
    Add ifelse2b.php to demonstrate ternary operator usage.

---
+++ tipTrikPemrogramanPhp/c1/ifelse2b.php
@@ -0,0 +1,9 @@
+<?php
+// Statement if else – operator ternary
+
+/* untuk menyelesaikan logika pemrograman.menggunakan operator ternary /
+/* trik sederhana dengan mendahulukan string */
+$pass = 'okey';
+exho ('oke' == $pass) ? 'OKE' : 'NOT OKE';
+// Hasil: NOT OKE
+?>
