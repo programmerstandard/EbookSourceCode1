acac2807cb80d098639bb1768dcd7aff300054c5
programmerbiasa <94773792+programmerbiasa@users.noreply.github.com>
2026-05-22 12:00:28 +0700

    Create array_unshift.php

---
+++ beginPhpMySql3rd/c5/array_unshift.php
@@ -0,0 +1,9 @@
+<?php
+// Adding and Removing Array Elements
+// 171 of 1080
+
+// Adding a Value to the Front of an Array
+$states = array("Ohio","New York");
+array_unshift($states,"California","Texas");
+// $states = array("California","Texas","Ohio","New York");
+?>
